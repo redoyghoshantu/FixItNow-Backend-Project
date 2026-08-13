4cf74b57eaede6f6d3516985d7e2d41fedd7be8e
Antu <theredoyghoshantu@gmail.com>
2026-08-13 22:26:07 +0600

    deploy the project:

---
+++ tsup.config.ts
@@ -0,0 +1,21 @@
+
+import {defineConfig} from 'tsup';
+
+export default defineConfig({
+    entry:['src/server.ts'],
+    format: ["esm"],
+    target:["esnext"],
+    platform: "node",
+    outDir:"dist",
+    bundle:true,
+    banner: {
+        js:`
+        
+        import {createRequire} from 'module';
+        const require = createRequire(import.meta.url);
+        
+        `
+        
+    }
+
+})
