eae92ce04d0942343b5c1c135b98262445d20920
Antu <theredoyghoshantu@gmail.com>
2026-08-13 20:12:22 +0600

    add app.ts

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "FixItNow API is running 🔧",
+  });
+});
+
+
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
+  });
+});
+
+export default app;
