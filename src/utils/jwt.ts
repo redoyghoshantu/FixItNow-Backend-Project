ff9c9aa05fd54ce40958ce38cf0e8c6c09f1baee
Antu <theredoyghoshantu@gmail.com>
2026-08-14 10:55:32 +0600

    add jwt.ts

---
+++ src/utils/jwt.ts
@@ -0,0 +1,13 @@
+import jwt, { SignOptions } from "jsonwebtoken";
+
+export const generateToken = (
+  payload: object,
+  secret: string,
+  expiresIn: SignOptions["expiresIn"]
+) => {
+  return jwt.sign(payload, secret, { expiresIn });
+};
+
+export const verifyToken = (token: string, secret: string) => {
+  return jwt.verify(token, secret);
+};
